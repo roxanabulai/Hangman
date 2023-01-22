068289fbac113b79d4f6495b45e7861117ea2c7b
Roxana Theodora <62172732+roxanabulai@users.noreply.github.com>
2023-01-22 15:43:01 +0000

    Correct flow

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ for _ in range(len(word)):
 nr_lines = len(word)
 nr_guess = 1
 end_game = False
-while not end_game and nr_guess <= 6:
+while not end_game and nr_guess < len(stages):
     guess = input("Guess a letter: ").lower()
     found = False
     for i in range(len(word)):
@@ -76,9 +76,11 @@ while not end_game and nr_guess <= 6:
             nr_lines = nr_lines - 1
             found = True
     if found == False:
-        nr_guess = nr_guess + 1
+      nr_guess = nr_guess + 1
+      print(stages[-nr_guess])
+    else:
+      print(stages[-nr_guess])
     print(word_g)
-    print(stages[-nr_guess])
     if nr_lines == 0:
         end_game = True
 if end_game == True:
